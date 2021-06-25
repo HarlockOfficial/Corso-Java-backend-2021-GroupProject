4e8a75dcbacab2709488ab44983bd95380974785
Giovanni <g.nunziante444@gmail.com>
2021-06-25 09:36:18 +0200

    istanze User in MainClass

--- Project_2/src/Main/MainClass.java
+++ Project_2/src/Main/MainClass.java
@@ -1,7 +1,16 @@
 package Main;
 
+import User.User;
+import User.Host;
+
 public class MainClass {
     public static void main(String[] args) {
+
+        User user1 = new User("Giggino","Rossi","giggino_rossi@gmail.com");
+        User user2 = new User("Giggino","Don Perignon","giggino_donperignon@gmail.com");
+        User host1 = new Host("Primo","Levi","primolevi@gmail.com");
+        User host2 = new Host("Italo","Calvino","italocalvino@gmail.com");
+        
 
     }
 }
